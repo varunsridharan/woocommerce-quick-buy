03c646997779e24cc1b0c178a49e0289b2226185
Varun <varunsridharan23@gmail.com>
2020-01-30 09:33:34 +0530

    Fixed Shortcode issue.

--- includes/shortcodes/class-link.php
+++ includes/shortcodes/class-link.php
@@ -45,7 +45,7 @@ if ( ! class_exists( '\WC_Quick_Buy\Shortcodes\Link' ) ) {
 
 			if ( $shortcode_product ) {
 				if ( $shortcode_product->is_in_stock() ) {
-					$quick_buy_link_product_types = Helper::option( 'quick_buy_link_product_types' );
+					$quick_buy_link_product_types = Helper::option( 'enabled_product_types' );
 					$quick_buy_link_product_types = ( ! is_array( $quick_buy_link_product_types ) ) ? array( 'simple' ) : $quick_buy_link_product_types;
 
 					/* @var \WC_Product $product */
