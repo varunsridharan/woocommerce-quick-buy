827995e1460cf8e4c84f05dcc7848e92f7795a31
Varun <varunsridharan23@gmail.com>
2020-06-12 08:32:05 +0530

    Fixed Shortcode URL Generator Issue.

--- includes/shortcodes/class-link.php
+++ includes/shortcodes/class-link.php
@@ -44,14 +44,13 @@ if ( ! class_exists( '\WC_Quick_Buy\Shortcodes\Link' ) ) {
 		protected function output() {
 			global $product;
 			$shortcode_product = ( empty( $this->options['product'] ) ) ? $product : wc_get_product( $this->options['product'] );
-
 			if ( $shortcode_product instanceof WC_Product && method_exists( $shortcode_product, 'is_in_stock' ) ) {
 				if ( $shortcode_product->is_in_stock() ) {
 					$quick_buy_link_product_types = Helper::option( 'enabled_product_types' );
 					$quick_buy_link_product_types = ( ! is_array( $quick_buy_link_product_types ) ) ? array( 'simple' ) : $quick_buy_link_product_types;
 
 					/* @var \WC_Product $product */
-					if ( in_array( $shortcode_product->get_type(), $quick_buy_link_product_types, true ) ) {
+					if ( in_array( 'all', $quick_buy_link_product_types ) || in_array( $shortcode_product->get_type(), $quick_buy_link_product_types, true ) ) {
 						$instance = new URL_Generator( array(
 							'product' => $shortcode_product->get_id(),
 							'qty'     => $this->option( 'qty' ),
